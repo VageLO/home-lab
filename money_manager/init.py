182d9d5147f8dbbaf3ff1036a6dcb2b11cde5bb0
VageLO <48802151+VageLO@users.noreply.github.com>
2024-11-21 23:03:29 +0300

    update: router

--- money_manager/init.py
+++ money_manager/init.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import project, account
+from .routers import project, account, categories
 from .logger import setupLogger
 
 setupLogger()
@@ -8,3 +8,4 @@ app = FastAPI()
 
 app.include_router(project.router)
 app.include_router(account.router)
+app.include_router(categories.router)
--- money_manager/routers/categories.py
+++ money_manager/routers/categories.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response, Depends, HTTPException
+from fastapi import APIRouter, Response, HTTPException
 from sqlalchemy.exc import IntegrityError
 from ..dependencies import SessionDep
 from ..core.models import (
@@ -8,8 +8,8 @@ from ..core.models import (
 )
 
 router = APIRouter(
-    prefix="/cagetory",
-    tags=["cagetory"],
+    prefix="/category",
+    tags=["category"],
     responses={404: {"description": "Not found"}},
 )
 
@@ -24,6 +24,17 @@ async def create_category(
     """
     new_category = Categories(parent_id=category.parent_id, title=category.title)
     session = db.session
-    try:
 
-    except 
+    try:
+        session.add(new_category)
+        session.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail='UNIQUE constraint failed')
+
+    session.refresh(new_category)
+
+    session.close()
+    db.engine.dispose()
+
+    response.status_code = 201
+    return new_category
